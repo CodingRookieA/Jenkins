aed25d1986b29661c017a3cbb9c4de1868f5c91c
Alexander Zhang <zhangalexander6@gmail.com>
2025-12-26 16:42:52 -0500

    added new test

--- test/test_calc.py
+++ test/test_calc.py
@@ -1,6 +1,6 @@
 # tests/test_calculator.py
 import pytest
-from app.calculator import add, divide
+from app.calculator import add, divide, sub
 
 def test_add():
     assert add(2, 3) == 5
@@ -11,3 +11,6 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
+
+def test_sub():
+    assert sub(100, 1) == 99
